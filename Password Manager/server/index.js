c9ce0f93c9790a7191c9c442b3654d344fcf3dd1
Ghadeer <qods_a@yahoo.com>
2022-11-10 16:28:36 -0500

    connecting express server to mysql PasswordManager database

--- Password Manager/server/index.js
+++ Password Manager/server/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
+const mysql = require('mysql');
+
+
+//all queries related to mysql - creating a connection
+const db = mysql.createConnection({
+    user: "ghadeer",
+    host: "localhost",
+    password: "Ghadeer10!",
+    database: "PasswordManager",
+});
+
 
 //setting up a route for initial page
 app.get("/", (req, res) => {
     res.send('server is up at port 3001');
 });
+
+
 
 
 app.listen(PORT, () => {
