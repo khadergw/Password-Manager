24152239d5a28956d280239d723ecc74e2c35729
Ghadeer <qods_a@yahoo.com>
2022-11-10 23:28:40 -0500

    styling the form and background

--- Password Manager/client/src/App.js
+++ Password Manager/client/src/App.js
@@ -3,22 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <div class="container"> 
+    <div className="container"> 
   <form >
     <p>Welcome to Your Password Manager</p>
     <input type="text" placeholder="Title" /><br/>
-    <input type="url" placeholder="Website url" /><br/>
+    <input type="url" placeholder="Website URL" /><br/>
     <input type="text" placeholder="User Name" /><br/>
     <input type="password" placeholder="Password" /><br/>
     <input type="button" value="Add Password" /><br/>
   </form>
 
-  <div class="drops">
-    <div class="drop drop-1"></div>
-    <div class="drop drop-2"></div>
-    <div class="drop drop-3"></div>
-    <div class="drop drop-4"></div>
-    <div class="drop drop-5"></div>
+  <div className="circles">
+    <div className="circle circle-1"></div>
+    <div className="circle circle-2"></div>
+    <div className="circle circle-3"></div>
+    <div className="circle circle-4"></div>
+    <div className="circle circle-5"></div>
   </div>
 
     </div>
