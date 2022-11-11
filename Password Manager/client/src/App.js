4a7f668c0de13698d62ae358b55247dd80e9e084
Ghadeer <qods_a@yahoo.com>
2022-11-10 22:18:24 -0500

    create the skeleton of the home page form

--- Password Manager/client/src/App.js
+++ Password Manager/client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container"> 
+  <form >
+    <p>Welcome to Your Password Manager</p>
+    <input type="text" placeholder="Title" /><br/>
+    <input type="url" placeholder="Website url" /><br/>
+    <input type="text" placeholder="User Name" /><br/>
+    <input type="password" placeholder="Password" /><br/>
+    <input type="button" value="Add Password" /><br/>
+  </form>
+
+  <div class="drops">
+    <div class="drop drop-1"></div>
+    <div class="drop drop-2"></div>
+    <div class="drop drop-3"></div>
+    <div class="drop drop-4"></div>
+    <div class="drop drop-5"></div>
+  </div>
+
+    </div>
     </div>
   );
 }
