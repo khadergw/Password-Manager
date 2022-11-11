1d41d4f6ed5e105e982b77438425d856c44d9156
Ghadeer <qods_a@yahoo.com>
2022-11-11 12:10:11 -0500

    setting up route for inserting values in db, and installing axios

--- Password Manager/server/index.js
+++ Password Manager/server/index.js
@@ -13,6 +13,23 @@ const db = mysql.createConnection({
 });
 
 
+//setting up an accessible route allows to insert the data in mysql
+app.post("/addpassword", (req, res) => {
+   //access the body(values in frontend) through req and pass them to backend
+   const {title, website, username, password} = req.body
+    
+   //passing sql query (insert) to insert the data in the db table
+   db.query("INSERT INTO passwords (title, website, username, password) VALUES (?,?,?,?)", [title, website, username, password],
+   (err, result)=> {
+    if(err) {
+        console.log(err);
+    } else {
+        res.send("Success");
+    }
+   })
+
+});
+
 //setting up a route for initial page
 app.get("/", (req, res) => {
     res.send('server is up at port 3001');
