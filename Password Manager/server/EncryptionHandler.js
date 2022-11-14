a075ee73ff6697a114d3254ac5320eb851cf39a0
Ghadeer <qods_a@yahoo.com>
2022-11-14 15:26:08 -0500

    create variable of 32 char to keep encryption safe

--- Password Manager/server/EncryptionHandler.js
+++ Password Manager/server/EncryptionHandler.js
@@ -1,9 +1,12 @@
 const crypto = require("crypto");
-
+//variable that keeps the encryotion safe consists from 32 char
+const secret = 'pppppppppppppppppppppppppppppppp';
+//encryption function
 const encrypt = (password) => {
 
 }
 
+//decryption function
 const decrypt = (encryption) => {
 
 }
