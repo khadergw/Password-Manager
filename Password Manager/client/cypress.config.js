c817a5f896fd949cfb4844be49bc1e1d8b21049d
Ghadeer <qods_a@yahoo.com>
2022-11-27 23:42:27 -0500

    enabling experimentalSTUDIO IN CYPRESS

--- Password Manager/client/cypress.config.js
+++ Password Manager/client/cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   component: {
+    experimentalStudio: "true",
+
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
@@ -15,9 +17,11 @@ module.exports = defineConfig({
     },
   },
 
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      experimentalStudio: true
     },
   },
 });
